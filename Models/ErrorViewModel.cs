c12f5445dfe376fb99b440c71fee882038f607f3
naveenkumar <naveenkumar@metamation.com>
2023-11-06 12:21:32 +0530

    Asp.net 7 MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HostingInAzure_GithubAspNet7.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
